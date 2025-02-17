e87f6d4cc72bd1f8344428dc2b3de7b24e497869
JayYadav <jay.shankar20@gmail.com>
2025-02-18 00:03:26 +0530

    route setup for users, posts, profile and auth

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get("/", (req, res) => {
   res.send("Hello, API is running!!!");
 });
 
+// DEfine routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
